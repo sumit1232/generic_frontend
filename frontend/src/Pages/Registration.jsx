9aef3d049b0b5fd4e872089ce43591d0a0484c25
Sumit Jadhav <sumitjadav2525@gmail.com>
2025-02-22 10:41:33 +0530

    frontEnd run

---
+++ frontend/src/Pages/Registration.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react'
+import axios from "axios"
+import { Link } from "react-router-dom"
+
+const Registration = () => {
+  const [registrationData, setRegistrationData] = useState({
+    username: '',
+    password: '',
+  })
+
+  const handleRegistrationchange = (e) => {
+    const { name, value } = e.target;
+    setRegistrationData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }))
+  }
+
+  const handleRegistrationSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:8000/posts', registrationData);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+    setRegistrationData({
+      username: '',
+      password: ''
+    })
+  }
+  return (
+    // <div>
+    //   <h1>Registration</h1>
+    //   <form onSubmit={handleRegistrationSubmit}>
+    //     <input
+    //       type='text'
+    //       name='username'
+    //       placeholder="username"
+    //       value={registrationData.username}
+    //       onChange={handleRegistrationchange}
+    //     />
+    //     <input
+    //       type='text'
+    //       name='password'
+    //       placeholder="password"
+    //       value={registrationData.password}
+    //       onChange={handleRegistrationchange}
+    //     />
+    //     <button
+    //       type='submit'
+    //     >Register</button>
+    //     <p>Already Register ?
+    //       <Link to="/login">Login</Link>
+    //     </p>
+    //   </form>
+    // </div>
+    <>
+      <div className="login-page">
+        <div className="form">
+          <form className="login-form" onSubmit={handleRegistrationSubmit}>
+            <input
+              type='text'
+              name='username'
+              placeholder="username"
+              value={registrationData.username}
+              onChange={handleRegistrationchange}
+            />
+            <input
+              type='text'
+              name='password'
+              placeholder="password"
+              value={registrationData.password}
+              onChange={handleRegistrationchange}
+            />
+            <button
+              type='submit'
+            >Register</button>
+            <p className="message">Already registered?<Link to="/">Login</Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Registration
